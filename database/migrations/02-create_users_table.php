21c64cb5a4be983efddd8802733b9e1c9ef06eff
ducmanhtechdev <supermoffcial@gmail.com>
2026-06-29 19:06:48 +0700

    init-project

---
+++ database/migrations/02-create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 40); 
+            $table->string("avatar", 100)->nullable(); 
+            $table->string('email', 60); 
+            $table->string('password', 80); 
+            $table->string('phonenumber', 10);
+            $table->foreignId('roleId')->constrained('roles');  // thiết lập khóa ngoại
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
