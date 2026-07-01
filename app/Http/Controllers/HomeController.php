6bcc0b018104b1b2c6155460d8a709e9d919c74e
Nguyen van Kien <kiennv.24itb@vku.udn.vn>
2026-07-01 22:48:24 +0700

    tee

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Role;
+use App\Models\User;
 
 
 class HomeController extends Controller
@@ -14,5 +15,9 @@ public function index () {
 
 public function about () {
     return view('about');
+}
+public function getname() {
+    $name = User::paginate(10);
+    return view('admin.dashboard', compact('name'));
 }
 }
