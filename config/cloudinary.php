4504e6983e5ff04e53b140399cce4b87d3630818
ducmanhtechdev <supermoffcial@gmail.com>
2026-07-03 21:26:56 +0700

    update cloudinary avatar with user avatar

---
+++ config/cloudinary.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    // 'cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'doi71x3cs'),
+    // 'api_key'    => env('CLOUDINARY_API_KEY', '237572357969624'),
+    // 'api_secret' => env('CLOUDINARY_API_SECRET', 'PqZm2poZ1bD7CHC5V_nxB2-FLes'),
+    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+    'api_key'    => env('CLOUDINARY_API_KEY'),
+    'api_secret' => env('CLOUDINARY_API_SECRET'),
+    'api_url'    => env('CLOUDINARY_URL'), // Dòng này quan trọng
+    'url' => 'cloudinary://237572357969624:PqZm2poZ1bD7CHC5V_nxB2-FLes@doi71x3cs',
+];
--- config/filesystems.php
+++ config/filesystems.php
@@ -57,13 +57,7 @@ return [
         ],
         
   'cloudinary' => [
-      'driver' => 'cloudinary',
-      'key' => env('CLOUDINARY_KEY'),
-      'secret' => env('CLOUDINARY_SECRET'),
-      'cloud' => env('CLOUDINARY_CLOUD_NAME'),
-      'url' => env('CLOUDINARY_URL'),
-      'secure' => (bool) env('CLOUDINARY_SECURE', true),
-      'prefix' => env('CLOUDINARY_PREFIX'),
+      'driver' => 'cloudinary'
   ],
 
     ],
