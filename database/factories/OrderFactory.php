da322c5f6ab8f9e27c765edc420f131fbf76fc7f
Nguyen van Kien <kiennv.24itb@vku.udn.vn>
2026-07-03 23:24:43 +0700

    fix lại factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -10,10 +10,9 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
             // Tạo mới một User hoặc lấy ngẫu nhiên ID của User đã có sẵn
-            'userId' => User::inRandomOrder()->first()?->id ?? User::factory(),
-            'status' => $this->faker->randomElement(['pending', 'shipping', 'completed', 'cancelled']),
+            'userId' =>User::factory(),
+            'status' => $this->faker->randomElement(['pending', 'processing', 'completed', 'canceled']),
             'created_at' => $this->faker->dateTimeBetween('-1 month', 'now'),
             'updated_at' => now(),
         ];
